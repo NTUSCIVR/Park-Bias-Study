f422dffe3f65d0e5309c823d823f069241455b97
Clem <ntuscivr@gmail.com>
2019-11-27 10:40:53 +0800

    Initial Commit

---
+++ Assets/Scripts/Sequence/Sequence.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sequence : MonoBehaviour {
+
+    [SerializeField]
+    MonoBehaviour[] m_components;
+
+    [SerializeField]
+    GameObject[] m_gameObjects;
+
+    public void Activate()
+    {
+        foreach (MonoBehaviour m in m_components)
+        {
+            m.enabled = true;
+        }
+
+        foreach (GameObject go in m_gameObjects)
+        {
+            go.SetActive(true);
+        }
+    }
+
+    public void Deactivate()
+    {
+        foreach (MonoBehaviour m in m_components)
+        {
+            m.enabled = false;
+        }
+
+        foreach (GameObject go in m_gameObjects)
+        {
+            go.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/Sequence/TeleportEnable.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class TeleportEnable : VRTK_DestinationPoint {
+
+    [SerializeField]
+    GameObjectRuntimeSet m_set;
+
+    bool disable = false;
+
+	// Update is called once per frame
+	void Update () {
+        base.Update();
+        if (disable)
+            return;
+        if (m_set.m_list.Count == 0)
+        {
+            disable = false;
+            enableTeleport = true;
+        }
+	}
+}
